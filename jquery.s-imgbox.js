19a347bfc1b80e6e30d2ae409cbe1ae3e97a972e
Sunyson <260851801@qq.com>
2015-01-12 17:22:16 +0800

    init

---
+++ jquery.s-imgbox.js
@@ -0,0 +1,14 @@
+;(function($){
+	$.fn.extend({
+		ssImgBox:function(options){
+			var options = $.extend({
+				t_width:40px, //缩略图宽高
+				o_width:300px,  //原始图宽度
+				o_height:400px  //原始图高度
+			},options);
+			return this.each(function(){
+
+			}); 
+		}
+	});
+})(jQuery);
---
+++ jquery.ss-imgbox.js
@@ -0,0 +1,12 @@
+;(function($){
+	$.fn.extend({
+		ssImgBox:function(options){
+			var options = $.extend({
+
+			},options);
+			return this.each(function(){
+
+			}); 
+		}
+	});
+})(jQuery);
